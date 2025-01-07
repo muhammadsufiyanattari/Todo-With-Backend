f3957e1b8a7a790e0413548fc9fd32b942b9209a
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2025-01-07 11:33:21 +0500

    up to date

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import axios from "axios";
 const App = () => {
   const base_url = "http://localhost:3000";
   const [todos, setTodos] = useState([]);
-  const [inptodo, setInptodo] = useState();
+  const [inptodo, setInptodo] = useState(null);
   const getTodo = async () => {
     try {
       const res = await axios(`${base_url}/getTodos`);
-      let todoData = res?.data.data;
+      let todoData = res?.data.data ;
+
       console.log(todoData);
     } catch (error) {
       console.log(error);
@@ -20,23 +21,22 @@ const App = () => {
   }, []);
   const inpvalue = (e) => {
     try {
-      
       e.preventDefault();
       let todo = e.target[0].value;
       console.log(todo);
-      setInptodo(todo)
+      setInptodo(todo);
       todo = e.target[0].value = "";
     } catch (error) {}
   };
-  useEffect(()=>{
-    inpvalue()
-  },[])
+  useEffect(() => {
+    inpvalue();
+  }, []);
 
   return (
     <>
       <div className="App flex flex-col items-center justify-center h-screen bg-gray-100">
         <h1>Todo App</h1>
-        <form onSubmit={inpvalue}  className="w-1/2">
+        <form onSubmit={inpvalue} className="w-1/2">
           <input
             type="text"
             className="w-full border border-gray-300 p-2 rounded"
@@ -45,11 +45,28 @@ const App = () => {
             Add Todo
           </button>
           <div className="bg-amber-700  text-black">
-{inptodo}
-
-</div>
+            {todos?.map((value, index) => {
+              return (
+                <div
+                  key={index}
+                  className="flex justify-between items-center p-2 border border-gray-300 mt-2"
+                >
+                  <div>{value.todoContent}</div>
+                 {console.log(value.todoContent)}
+                 
+                  <div>
+                    <button className="bg-green-500 text-white p-2 rounded">
+                      Edit
+                    </button>
+                    <button className="bg-red-500 text-white p-2 rounded">
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </form>
-       
       </div>
     </>
   );
