414b96a716390c041bf31744c034969ff2a9e2fe
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2025-01-07 09:25:43 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 const App = () => {
   const base_url = "http://localhost:3000";
   const [todos, setTodos] = useState([]);
-  const getTodo=async()=>{
-    const res=await axios(`${base_url}/getTodos`)
-    console.log(res);
-    
-
-  }
-  getTodo()
-  const inpvalue =  (e) => {
+  const [inptodo, setInptodo] = useState();
+  const getTodo = async () => {
     try {
+      const res = await axios(`${base_url}/getTodos`);
+      let todoData = res?.data.data;
+      console.log(todoData);
+    } catch (error) {
+      console.log(error);
+      setTodos(todoData);
+    }
+  };
+  useEffect(() => {
+    getTodo();
+  }, []);
+  const inpvalue = (e) => {
+    try {
+      
       e.preventDefault();
       let todo = e.target[0].value;
-      // console.log(todo);
+      console.log(todo);
+      setInptodo(todo)
       todo = e.target[0].value = "";
     } catch (error) {}
   };
+  useEffect(()=>{
+    inpvalue()
+  },[])
 
   return (
     <>
       <div className="App flex flex-col items-center justify-center h-screen bg-gray-100">
         <h1>Todo App</h1>
-        <form onSubmit={inpvalue} className="w-1/2">
+        <form onSubmit={inpvalue}  className="w-1/2">
           <input
             type="text"
             className="w-full border border-gray-300 p-2 rounded"
@@ -32,10 +44,12 @@ const App = () => {
           <button className="w-full bg-blue-500 text-white p-2 rounded mt-2">
             Add Todo
           </button>
+          <div className="bg-amber-700  text-black">
+{inptodo}
+
+</div>
         </form>
-        <div>
-          <input type="text" name="" id="" />
-        </div>
+       
       </div>
     </>
   );
