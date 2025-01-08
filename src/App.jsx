9819bec727f3530cfd28fcb01f84e4df3630a829
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2025-01-09 01:58:42 +0500

    added

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,22 @@ import axios from "axios";
 const App = () => {
   const base_url = "http://localhost:3000";
   const [todos, setTodos] = useState([]);
-  const [inptodo, setInptodo] = useState(null);
+  // const [inptodo, setInptodo] = useState(null);
   const getTodo = async () => {
     try {
       const res = await axios(`${base_url}/getTodos`);
-      let todoData = res?.data?.data.map(todo => ({
-        ...todo,
-        todoContent: todo.todoContent || "No Content"
-      }));
-
-      console.log("todoData",todoData);
+      // let todoData = res?.data?.data.map((todo) => ({
+      //   ...todo,
+      //   todoContent: todo.todoContent || "No Content",
+      //   id: todo.id || "No id",
+        
+      // }));
+      let todoData = res?.data?.data;
+      // todoData.map((todo) => (
+      //  console.log("todo", todo.todoContent , todo.id)
+       
+      // ));
+      console.log("todoData", todoData);
       setTodos(todoData);
     } catch (error) {
       console.log(error);
@@ -24,7 +30,7 @@ const App = () => {
   }, []);
   // const inpvalue = (e) => {
   //   try {
-     
+
   //     let todo = e.target[0].value;
   //     console.log(todo);
   //     setInptodo(todo);
@@ -35,36 +41,52 @@ const App = () => {
   //   inpvalue();
   // }, []);
 
+
+  //post request
   const addTodo = async (event) => {
-    try {  
-        event.preventDefault();
+    try {
+      event.preventDefault();
 
       let inpValue = event.target[0].value;
-      const res = await axios.post(`${base_url}/addTodo`,
-        {
-          "todo": inpValue
+      const res = await axios.post(`${base_url}/addTodo`, {
+        todo: inpValue,
       });
-      console.log(res);
-      getTodo();
+      // console.log(res);
+      getTodo(); //page refresh karne par value mil rhi thi is lye hume function ko call krna para
+      event.target.reset(); //ye form ko clear krne k liye use hota hai
     } catch (error) {
       console.log(error);
     }
+  };
+  // delete requset
+  const deleteTodo = async (todoId) => {
+    // console.log("todoId", todoId);
+   try {
+    const {data}= axios.delete(`${base_url}/deletTodo/${todoId}`)
+// console.log("data", data);
+    getTodo();
+   } catch (error) {
+    console.log(error);
+   }
   }
-
   return (
     <>
       <div className="App flex flex-col items-center justify-center h-screen bg-gray-100">
-        <h1>Todo App</h1>
-        <form onSubmit={addTodo} className="w-1/2 overflow-scroll">
+        <h1 className="font-extrabold text-3xl py-1">Todo App</h1>
+        <form onSubmit={addTodo} className="w-1/2 overflow-auto">
           <input
+          required
             type="text"
             className="w-full border border-gray-300 p-2 rounded"
           />
-          <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded mt-2">
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white p-2 rounded mt-2"
+          >
             Add Todo
           </button>
-          <div className="bg-amber-700  text-black">
-            {!todos?.length  && <div>No Todos</div>}
+          <div className="bg-amber-7  text-black">
+            {!todos?.length && <div className="flex justify-center items-center font-bold">No Todos</div>}
             {todos?.map((value, index) => {
               return (
                 <div
@@ -72,14 +94,15 @@ const App = () => {
                   className="flex justify-between items-center p-2 border border-gray-300 mt-2"
                 >
                   <div>{value.todoContent}</div>
-                 {console.log(value.todoContent)}
-                 
+                  {/* {console.log(value.todoContent)} */}
+
                   <div>
                     <button className="bg-green-500 text-white p-2 rounded">
                       Edit
                     </button>
-                    <button className="bg-red-500 text-white p-2 rounded">
-                      Delete
+                    <button onClick={()=>deleteTodo(value.id)} className="bg-red-500 text-white p-2 rounded">
+                      
+                      Delete 
                     </button>
                   </div>
                 </div>
